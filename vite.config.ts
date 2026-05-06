b394455c50989fb32c2882635a78f2e4738e8d56
dariush-hassani <dariush.hassani@outlook.com>
2026-05-06 15:04:00 +0330

    feat: change whole project :)

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "index.ts"),
+      name: "CockpitCharts",
+      fileName: "cockpit-charts",
+    },
+    rollupOptions: {
+      external: [/^d3-.*/],
+    },
+  },
+  plugins: [dts(), tailwindcss()],
+});
